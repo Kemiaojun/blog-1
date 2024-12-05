12875756941ab7d105b6b2a1a7aa82d972085b65
lumianya <kemiaodwi@gmail.com>
2024-12-05 19:41:38 +0800

    Update page.tsx

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
             <Dot className='cursor-not-allowed bg-green-500' />
           </span>
           <span className='text-center text-gray-400'>
-            zhangyu@MacBook-Air:~
+            克喵爱吃凉面:~
           </span>
           <span className='text-end text-gray-500'>⌥⌘1</span>
         </header>
